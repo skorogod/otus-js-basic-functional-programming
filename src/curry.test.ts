2314007378c5d8b88aa00b57012693c865aeac7b
Sergey <mail@vasya.com>
2023-10-25 01:12:57 +0300

    curry function added

---
+++ src/curry.test.ts
@@ -0,0 +1,39 @@
+const { curry } = require("./curry")
+
+let sum: Function;
+let hof: Function;
+
+beforeAll (() => {
+    sum = (a, b, c, d, e) => a + b + c + d + e;
+})
+
+beforeEach (() => {
+    hof = curry(sum);
+})
+
+describe("curry", () => {
+    test("curry is a function", () => {
+        expect(curry).toBeInstanceOf(Function);
+    })
+
+    test("hof(1,2,3,4,5) = 15", () => {
+        expect(hof(1,2,3,4,5)).toBe(15);
+    })
+
+    test("hof(2, 3, 4)(5, 6)) = 20", () => {
+        expect(hof(2, 3, 4)(5, 6)).toBe(20);
+    })
+
+    test("(hof(3, 4)(5, 6)(7) = 25", () => {
+        expect(hof(3, 4)(5, 6)(7)).toBe(25);
+    })
+
+    test("hof(4, 5)(6)(7, 8) = 30", () => {
+        expect(hof(4, 5)(6)(7, 8)).toBe(30);
+    })
+
+    test("hof(5)(6)(7)(8)(9) = 35", () => {
+        expect(hof(5)(6)(7)(8)(9)).toBe(35);
+    })
+    
+})
---
+++ src/curry.ts
@@ -0,0 +1,11 @@
+export function curry(func: Function) {
+    return function f(...args) {
+      if (args.length >= func.length) {
+        return func.apply(this, args);
+      } else {
+        return function(...args2) {
+          return f.apply(this, args.concat(args2));
+        }
+      }
+    };
+  }  
